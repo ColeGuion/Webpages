bc57b9a1b5a341caa6c28de4382dd98eeba213a8
Cole.Guion <cole.guion@goldkey.com>
2025-04-23 11:13:55 -0500

    add docs.html

--- Scripts/todo.js
+++ Scripts/todo.js
@@ -1,12 +1,23 @@
 let descriptionVisible = false;
 let isWhiteBackground = true;
 
-function LoadData() {
-    const savedToDos = JSON.parse(localStorage.getItem('todoList')) || [];
-    savedToDos.forEach(todo => {
-        AddToList(todo.text, todo.description);
-    });
-}
+/* TODO LIST WEBPAGE:
+    - Input bar for adding todo items
+        - Add button to add todo item
+        - Button to add a description list (such as in "Projects to Build" in todays notes)
+        - Optional category to add to item
+            - Make the category & description addable to an item later 
+    - List of todo items below input bar
+        - Each item should have a checkbox to mark as done
+        - Each item should have a button to delete
+            - Undo deletions
+        - Each item should have a button to edit
+        - Each item should have a button to add a description list
+    - Sort list by category
+    - Make description collapsible and show once the title is clicked
+
+(Maybe turn into public site & profit!)
+*/
 
 document.addEventListener("DOMContentLoaded", function () {
     // Initialize to-do list from localStorage if it exists
@@ -61,6 +72,16 @@ function AddToDo() {
         document.getElementById('todo-input').value = '';
         document.getElementById('description-input').value = '';
     }
+
+    // Hide description if visible
+    if (descriptionVisible) {
+        toggleDescription();
+    }
+
+    // Add current date and time to the to-do item
+    const currentDate = new Date();
+    const formattedDate = currentDate.toLocaleString();
+    console.log(`To-Do added on: ${formattedDate}`);
 }
 
 // Function to add to-do to the list and apply background
@@ -98,6 +119,19 @@ function AddToList(task, todoDescription) {
     deleteBtn.onclick = () => {
         listItem.remove(); // Remove task from DOM
         saveToLocalStorage(); // Update localStorage
+
+        // Get removed date and time
+        const currentDate = new Date();
+        const remDate = currentDate.toLocaleString();
+        console.log(`Removed task on: ${remDate}`);
+
+        // Add deleted tasks to saved array
+        let delTasks = JSON.parse(localStorage.getItem('removedTasks')) || [];
+        //let delTasks = [];
+        delTasks.push({text: task, description: todoDescription, date: remDate})
+        
+        // Save the updated array of removed tasks
+        localStorage.setItem('removedTasks', JSON.stringify(delTasks));
     };
 
     // Add title and description to the list item
@@ -107,6 +141,47 @@ function AddToList(task, todoDescription) {
 
     // Add list item to the todo list
     todoList.appendChild(listItem);
+}
+
+function toggleOldTasks() {
+    const todoList = document.getElementById('todo-list');
+    todoList.innerHTML = "";
+    
+    const deleted_tasks = JSON.parse(localStorage.getItem('removedTasks')) || [];
+    deleted_tasks.forEach(task => {
+        //! Don't add deletion button for these tasks
+        AddToList(task.text, task.description);
+    });
+}
+
+// Toggle visibility of the description textarea
+function toggleDescription() {
+    const descriptionInput = document.getElementById('description-input');
+    const descriptionBtn = document.getElementById('descBtn');
+    
+    descriptionVisible = !descriptionVisible;
+    if (descriptionVisible) {
+        descriptionInput.style.display = 'block';
+        descriptionBtn.innerText = 'Hide Description';
+    } else {
+        descriptionInput.style.display = 'none';
+        descriptionBtn.innerText = 'Add Description';
+    }
+    /* descriptionInput.value = '- '; */
+}
+
+// Toggle background color of .addBar
+function toggleBackground() {
+    const addBar = document.querySelector('.addBar');
+    isWhiteBackground = !isWhiteBackground;
+    addBar.style.backgroundColor = isWhiteBackground ? 'white' : 'wheat';
+}
+
+function LoadData() {
+    const savedToDos = JSON.parse(localStorage.getItem('todoList')) || [];
+    savedToDos.forEach(todo => {
+        AddToList(todo.text, todo.description);
+    });
 }
 
 // Save the current to-do list to localStorage
@@ -119,19 +194,4 @@ function saveToLocalStorage() {
         return { text: title, description: description, backgroundColor: backgroundColor };
     });
     localStorage.setItem('todoList', JSON.stringify(todoListArray));
-}
-
-// Toggle visibility of the description textarea
-function toggleDescription() {
-    const descriptionInput = document.getElementById('description-input');
-    descriptionVisible = !descriptionVisible;
-    descriptionInput.style.display = descriptionVisible ? 'block' : 'none';
-    /* descriptionInput.value = '- '; */
-}
-
-// Toggle background color of .addBar
-function toggleBackground() {
-    const addBar = document.querySelector('.addBar');
-    isWhiteBackground = !isWhiteBackground;
-    addBar.style.backgroundColor = isWhiteBackground ? 'white' : 'wheat';
 }
