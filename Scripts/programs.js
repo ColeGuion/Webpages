33eb5b71c4114eede7f926aa655c539ffbda4bfe
Cole.Guion <cole.guion@goldkey.com>
2025-10-09 09:27:17 -0500

    update

--- Scripts/programs.js
+++ Scripts/programs.js
@@ -1,38 +1,66 @@
-
 const Linux_Commands = [
     //["COMMAND", "DESCRIPTION"],
     //["chmod +x script.sh", "Make a script executable"],
     //["./script.sh", "Run a script in the current directory"],
     //["bash script.sh", "Run a bash script in the current directory"],
     //["source script.sh", "Run a bash script in the current shell (keeps any changes to env variables)"],
-    ["alias ll='ls -lh'", "Create a bash alias `ll` that runs `ls -lh`"],
-    ["wget URL", "Download a file"],
-    ["ps -eo %cpu,comm,cmd,pid --sort=-%cpu | head -n 8", "Display the first 8 running processes sorted by cpu usage"],
-    ["ps -eo pid,comm,%cpu --sort=-%cpu | awk 'NR==1 || $3>0.5'", "Running processes using more than .5% of the CPU (`NR==1` displays headings)"],
-    ["sudo apt upgrade -y", "Upgrades all installed packages without asking for confirmation"],
-    ["tree -L 2", "Show directory structure up to 2 levels deep"],
-    ["find . -type f -size +100M", "Find files larger than 100MB in current directory"],
-    ["find ./Models -type f -name \"tokenizer.json\" | wc -l", "Find the amount of `tokenizer.json` files anywhere inside the `/Models` directory"],
-    ["mv f1.txt newFile.txt", "Rename `f1.txt` to `newFile.txt`"],
-    ["mv f1.txt dir", "Move `f1.txt` to the specified directory"],
-    ["!$", "Last argument of previous command"],
-    ["!!", "Repeat last command"],
-    ["cd ~", "Home Directory"],
-    ["cd -", "Previous Directory"],
-    ["hostname -I", "IP Address"],
-    ["pip install -r requirements.txt", "Install pip modules from requirements.txt file"],
-    ["pip list --format=freeze > requirements.txt", "Create requirements.txt file from current pip modules"],
-    ["ps aux | head -n 5", "View the first 5 running processes"],
-    ["pip list | grep -E '^(tr|opt)|onnx'", "List pip modules that start with <em>\"tr\"</em> or <em>\"opt\"</em> OR contain the string <em>\"onnx\"</em>"],
-    ["pip list | grep tr", "List pip modules that contain <em>\"tr\"</em>"],
-    ["ls -l FILE", "View file permissions"],
-    ["source ~/.bashrc", "Apply the changes to ~/.bashrc",],
-    ["nano ~/.bash_aliases", "Edit linux bash script for aliases",],
     ["nano ~/.bashrc", "Edit linux bash profile"],
+    ["nano ~/.bash_aliases", "Edit linux bash script for aliases",],
+    ["source ~/.bashrc", "Apply the changes to ~/.bashrc",],
+    ["ls -l FILE", "View file permissions"],
+    ["pip list | grep tr", "List pip modules that contain <em>\"tr\"</em>"],
+    ["pip list | grep -E '^(tr|opt)|onnx'", "List pip modules that start with <em>\"tr\"</em> or <em>\"opt\"</em> OR contain the string <em>\"onnx\"</em>"],
+    ["ps aux | head -n 5", "View the first 5 running processes"],
+    ["pip list --format=freeze > requirements.txt", "Create requirements.txt file from current pip modules"],
+    ["pip install -r requirements.txt", "Install pip modules from requirements.txt file"],
+    ["hostname -I", "IP Address"],
+    ["cd -", "Previous Directory"],
+    ["cd ~", "Home Directory"],
+    ["!!", "Repeat last command"],
+    ["!$", "Last argument of previous command"],
+    ["mv f1.txt dir", "Move `f1.txt` to the specified directory"],
+    ["mv f1.txt newFile.txt", "Rename `f1.txt` to `newFile.txt`"],
+    ["find ./Models -type f -name \"tokenizer.json\" | wc -l", "Find the amount of `tokenizer.json` files anywhere inside the `/Models` directory"],
+    ["find . -type f -size +100M", "Find files larger than 100MB in current directory"],
+    ["tree -L 2", "Show directory structure up to 2 levels deep"],
+    ["sudo apt upgrade -y", "Upgrades all installed packages without asking for confirmation"],
+    ["ps -eo pid,comm,%cpu --sort=-%cpu | awk 'NR==1 || $3>0.5'", "Running processes using more than .5% of the CPU (`NR==1` displays headings)"],
+    ["ps -eo %cpu,comm,cmd,pid --sort=-%cpu | head -n 8", "Display the first 8 running processes sorted by cpu usage"],
+    ["wget URL", "Download a file"],
+    ["alias ll='ls -lh'", "Create a bash alias `ll` that runs `ls -lh`"],
+    ["<span>man ps</span><br>$ <span>ps --help</span><br>$ <span>ps --help all</span><br>$ <span>info ps</span>", "Get Help for the Linux `ps` command"],
+
+    // "Memory Management" Section
+    ["HEADER", "Memory Management", "lx-memory"], // [HEADER, Name, id]
+    ["ls -lh", "Get the size of every file in a folder"],
+    ["df -h", "Available disk space"],
+    ["du -sh /home/tech/Downloads/", "Get the size of the Downloads folder"],
+    ["du -h --max-depth=1 /Models | sort -hr", "List the sizes of the largest folders in `/Models`<br>Change to `du -ah` to list files too"],
+    ["du -ah ~ | sort -rh | head -20", "Check large files in home directory"],
+
+    // "Screen" Section
+    ["HEADER", "Screen", "lx-screen"], 
+    ["screen", "Start a new screen session"],
+    ["screen -ls", "List available screens"],
+    ["screen -s <name>", "Start a new named screen session"],
+    ["screen -r <ID or name>", "Reattach a detached screen"],
+    ["screen -d <ID or name>", "Detach a running screen session remotely"],
+    ["screen -X -S <ID or name> quit", "Kill a specific screen session"],
+    ["screen -wipe", "Remove dead (terminated screen sessions)"],
+
+    // "Advanced & GPU" Section
+    ["HEADER", "Advanced & GPU", "lx-gpu"], 
+    ["nvtop", "GPU Usage"],
+    ["htop", "CPU Usage"],
+    ["mokutil --sb-state", "Check if secure boot is enabled"],
+    ["sudo systemctl reboot --firmware-setup", "Reboot directly into secure boot"],
+    ["uname -r", "Get the kernel version"],
+    ["lspci | grep -i vga", "List GPU driver (GeForce 2080)"],
+    ["lsmod | grep nvidia", "List nvidia stuff (drivers? modules?)"],
 ];
 
 
-
+let cole=true;
 const Python_Commmands = [
     {
         Description: "List Indexing",
@@ -41,7 +69,7 @@ const Python_Commmands = [
             x[:<b class="idx">3</b>]   # [1, 2, 3]
             x[<b class="idx">3</b>:]   # [4, 5, 6, 7, 8, 9, 10]
 
-            x[<b class="idx">3:6</b>]  # [4, 5, 6]
+            x[<b class="idx">3</b>:<b class="idx">6</b>]  # [4, 5, 6]
             x[<b class="idx">-3</b>:]  # [8, 9, 10]
             x[:<b class="idx">-3</b>]  # [1, 2, 3, 4, 5, 6, 7]
 
@@ -172,28 +200,40 @@ function addCommands() {
 
     // Get the header row (first row)
     const headerRow = table.rows[0];
+    console.log(headerRow);
+    console.log(headerRow.parentNode.parentNode);
+    console.log(headerRow.nextSibling);
     
+    Linux_Commands.reverse();
     Linux_Commands.forEach(command => {
         const row = document.createElement('tr');
-        
-        const commandCell = document.createElement('td');
-        commandCell.className = 'commands';
-        const commandSpan = document.createElement('span');
-        commandSpan.textContent = command[0];
-        commandCell.appendChild(commandSpan);
-        
-        const descCell = document.createElement('td');
-        descCell.className = 'description';
-        descCell.innerHTML = command[1];
-        
-        row.appendChild(commandCell);
-        row.appendChild(descCell);
+
+        if (command[0] === "HEADER") {
+            // Create a header row
+            row.id = command[2];
+            row.innerHTML = `<td colspan="2" class="tabSect">${command[1]}</td>`;
+        } else {
+            const commandCell = document.createElement('td');
+            commandCell.className = 'commands';
+            if (command[0].startsWith("<span>")) {
+                commandCell.innerHTML = command[0];
+            } else {
+                const commandSpan = document.createElement('span');
+                commandSpan.textContent = command[0];
+                commandCell.appendChild(commandSpan);
+            }
+            
+            const descCell = document.createElement('td');
+            descCell.className = 'description';
+            descCell.innerHTML = command[1];
+
+            row.appendChild(commandCell);
+            row.appendChild(descCell);
+        }
         
         // Insert after the header row but before the existing rows
-        //table.insertBefore(row, table.rows[1]);
+        //headerRow.parentNode.parentNode.appendChild(row);
         headerRow.parentNode.insertBefore(row, headerRow.nextSibling);
-        
-        
     });
 }
 
